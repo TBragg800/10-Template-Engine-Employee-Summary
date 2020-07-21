27088fe6cc51a7e88e3a835028a769ed4fdd8d39
LM&T <tbbragg83@gmail.com>
2020-07-21 13:57:28 -0500

    comments

--- app.js
+++ app.js
@@ -10,30 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
+// empty array to hold employees
 const employeesArray = [];
 
 // array of questions for user
@@ -89,6 +66,7 @@ const questions = [
     }
 ];
 
+// question array to handle additional employees for user
 const newEmpQues = [
     {
         type: "confirm",
@@ -96,19 +74,6 @@ const newEmpQues = [
         name: "new"
       }
 ];
-
-// function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, data, (err) => {
-
-//         if (err) {
-//           return console.log(err);
-//         }
-
-//         console.log("Success!");
-      
-//       });
-// }
 
 // function to initialize program
 function init() {
@@ -129,10 +94,13 @@ function init() {
             var newManager = new Manager(name, id, email, answers.officeNumber);
             employeesArray.push(newManager);
         }
+// function call for additional employees or to finalize document.
         init2();
     });
 };
 
+// function to prompt for additional employees and if else statement to finish prompts and
+// write to file.
 function init2() {
     inquirer.prompt(newEmpQues).then(newAnswers => {
         if (newAnswers.new) {
